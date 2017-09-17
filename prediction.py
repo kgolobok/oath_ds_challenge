3558a865006a94209a2c1be28f97982cfe7f145c
kgolobok <kgolobok@umich.edu>
2017-09-17 14:44:48 -0400

    Use mfcc features

--- prediction.py
+++ prediction.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-import os, sys, sklearn, scipy
+import os, sys, sklearn, scipy, librosa
 
 
 from optparse import OptionParser
@@ -12,11 +12,29 @@ from scipy.stats import skew, kurtosis
 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
 
 
 label_mapping = { 'BassClarinet':0, 'BassTrombone':1, 'BbClarinet':2, 'Cello':3, 'EbClarinet':4, 'Marimba':5, 'TenorTrombone':6, 'Viola':7, 'Violin':8, 'Xylophone':9 }
 rlabel_mapping = { 0:'BassClarinet', 1:'BassTrombone', 2:'BbClarinet', 3:'Cello', 4:'EbClarinet', 5:'Marimba', 6:'TenorTrombone', 7:'Viola', 8:'Violin', 9:'Xylophone' }
+
+def calculate_stats( data, a ):
+
+	mean = np.mean( data, axis=a )
+	var = np.var( data, axis=a )
+	skewness = skew( data, axis=a )
+	kurt = kurtosis( data, axis=a )
+	p5 = np.percentile( data, 5, axis=a )
+	p25 = np.percentile( data, 25, axis=a )
+	p50 = np.percentile( data, 50, axis=a )
+	p75 = np.percentile( data, 75, axis=a )
+	p95 = np.percentile( data, 95, axis=a )
+
+	# print( '{} {} {} {} {} {} {} {} {}'.format( mean.shape, var.shape, skewness.shape, kurt.shape, p5.shape, p25.shape, p50.shape, p75.shape, p95.shape ) )
+
+	return np.hstack(( mean, var, skew, p5, p25, p50, p75, p95 ))	
+
+
 
 def get_features( in_path ):
 
@@ -32,22 +50,17 @@ def get_features( in_path ):
 		print( '\tProcessing {}'.format( file ) )
 
 		rate, samples = wavfile.read( os.path.join( in_path, file ) )
+
+		# Each ow corresponds to a sequence for same coefficient
+		mfcc_mat = librosa.feature.mfcc( y=samples, sr=rate )
 		fft = abs( scipy.fft( samples ) )
 
 		curr_X = np.zeros([ 1, num_features ])
 		curr_y = np.zeros([1])
 
-		curr_X[0,0] = np.mean( fft )
-		curr_X[0,1] = np.var( fft )
-		curr_X[0,2] = skew( fft )
-		curr_X[0,3] = kurtosis( fft )
-		curr_X[0,4] = np.min( fft )
-		curr_X[0,5] = np.max( fft )
-		curr_X[0,6] = np.percentile( fft, 5 )
-		curr_X[0,7] = np.percentile( fft, 25 )
-		curr_X[0,8] = np.percentile( fft, 50 )
-		curr_X[0,9] = np.percentile( fft, 75 )
-		curr_X[0,10] = np.percentile( fft, 95 )
+
+		curr_X = calculate_stats( mfcc_mat, 1 )
+		# curr_X.append()
 
 		curr_y = label_mapping[file.split('_')[0]]
 
@@ -70,6 +83,8 @@ def test_classifier( rf, X_test, y_test ):
 	y_hat = rf.predict( X_test )
 	mat = confusion_matrix( y_test, y_hat )
 	print( 'Confusion matrix:\n{}'.format( mat ) )
+
+	print( 'Accuracy score: {}'.format( accuracy_score( y_test, y_hat ) ) )
 
 
 	pass
